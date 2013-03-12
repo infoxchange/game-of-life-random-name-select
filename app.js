434ad7cfa16e2cdb54e16609ac9f661c9719e866
Tom Broerse <tom@infoxchange.net.au>
2013-03-12 14:53:22 +1100

    Basic Express 'Hello World' app.

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+  var body = 'Hello World';
+  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Length', body.length);
+  res.end(body);
+});
+
+app.listen(3000);
+console.log('Listening on port 3000');
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.send(500, 'Something broke!');
+});
