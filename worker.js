9cf81ed2dd2df1e1ab323abb0f0ce069865346a6
Alexey Kotlyarov <alexey@infoxchange.net.au>
2013-03-12 15:14:55 +1100

    Indentation.

--- worker.js
+++ worker.js
@@ -10,64 +10,64 @@ var state = {}; // hash by user ID, then array by cycle number, then bit matrix
 var queue = []; // array of { user: string, cycle: integer }
 
 function addjob(input) {
-    var user = input.user;
-    var data = input.data;
-    state[user] = [data];
-    queue.push({ user: user, cycle: 0 });
+  var user = input.user;
+  var data = input.data;
+  state[user] = [data];
+  queue.push({ user: user, cycle: 0 });
 }
 
 function last(user) { // return { cycle: integer, data: bit matrix }
-    var cycle = state[user].length - 1;
-    return state[user][cycle];
+  var cycle = state[user].length - 1;
+  return state[user][cycle];
 }
 
 function run() {
-    var job = queue.shift();
-    var user = job.user;
-    var cycle = job.cycle;
-    var data = state[user][cycle];
-    var newdata = process(data);
-    cycle++;
-    state[user][cycle] = newdata;
-    queue.push({ user: user, cycle: cycle });
+  var job = queue.shift();
+  var user = job.user;
+  var cycle = job.cycle;
+  var data = state[user][cycle];
+  var newdata = process(data);
+  cycle++;
+  state[user][cycle] = newdata;
+  queue.push({ user: user, cycle: cycle });
 }
 
 function n(data, x, y) {
-    var r = data[y];
-    if (typeof r === 'undefined') return 0;
-    r = r[x];
-    return typeof r === 'undefined' ? 0 : r;
+  var r = data[y];
+  if (typeof r === 'undefined') return 0;
+  r = r[x];
+  return typeof r === 'undefined' ? 0 : r;
 }
 
 function process(data) {
-    var height = data.length;
-    var width = data[0].length;
-    var newdata = Array(height);
-    for (var y = 0; y < height; y++) {
-        newdata[y] = Array(width);
-        for (var x = 0; x < width; x++) {
-            var around =
-                n(data, x - 1, y - 1) +
-                n(data, x - 1, y    ) +
-                n(data, x - 1, y + 1) +
-                n(data, x    , y - 1) +
-                n(data, x    , y + 1) +
-                n(data, x + 1, y - 1) +
-                n(data, x + 1, y    ) +
-                n(data, x + 1, y + 1);
+  var height = data.length;
+  var width = data[0].length;
+  var newdata = Array(height);
+  for (var y = 0; y < height; y++) {
+    newdata[y] = Array(width);
+    for (var x = 0; x < width; x++) {
+      var around =
+        n(data, x - 1, y - 1) +
+        n(data, x - 1, y  ) +
+        n(data, x - 1, y + 1) +
+        n(data, x  , y - 1) +
+        n(data, x  , y + 1) +
+        n(data, x + 1, y - 1) +
+        n(data, x + 1, y  ) +
+        n(data, x + 1, y + 1);
 
-            var res;
-            if (around < 2 || around > 3)
-                res = 0;
-            else if (around == 3)
-                res = 1;
-            else
-                res = data[y][x];
+      var res;
+      if (around < 2 || around > 3)
+        res = 0;
+      else if (around == 3)
+        res = 1;
+      else
+        res = data[y][x];
 
-            newdata[y][x] = res;
-        }
+      newdata[y][x] = res;
     }
-    return newdata;
+  }
+  return newdata;
 }
 
 exports.addjob = addjob;
