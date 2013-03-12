9b5aa93a512f3c2d51f92b48ffd20556e752434b
Alex Hutton <ahutton@infoxchange.net.au>
2013-03-12 17:57:31 +1100

    test table for gol

--- public/js/display.js
+++ public/js/display.js
@@ -1,14 +1,15 @@
 var width, height;
+width = 100;
+height = 100;
+
 function initRaster(selector) {
-	width = 100;
-	height = 100;
+	var table;
 
 	table = '<table><tbody>';
 
-	for (var j = 0; j < height; j++)
-		table += '<tr>'; 
-		for (var i = 0; i < width; i++)
-		{
+	for (var j = 0; j < height; j++) {
+		table += '<tr>';
+		for (var i = 0; i < width; i++)	{
 			table += '<td class="' + 'pixel-' + i + '-' + j + '"></td>';
 		}
 		table += '</tr>';
@@ -22,6 +23,10 @@ function updateRaster(selector, data){
 	var pos = 0;
 
 	$('td', selector).each(function(){
-		
+		var i, j;
+		i = pos % width;
+		j = Math.floor(pos / height);
+		$(this).css('background-color', ((data[j][i] == '1') ? 'white' : 'black'));
+		pos++;
 	});
 }
