08538ce8820e681536dea931677b1bbb0e044d67
Ricky Cook <ricky@infoxchange.net.au>
2013-03-12 17:15:25 +1100

    Fixed config

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
   cookieSecret: 'change-me',
   port: 3000,
-  baseURL: 'http://localhost:3000'
+  baseURL: 'http://localhost:3000',
   googleApi: {
     clientID:     '104017991848.apps.googleusercontent.com',
     clientSecret: 'h8Lqcf_NwcsLzvPcIMIYWGF-'
